71ead2289f0b0e6499dcf95c41a954df411e0fd7
nazna <14846330+nazna@users.noreply.github.com>
2017-12-07 14:17:45 +0900

    add: データセットを作成するプログラムとsplitされたCSVファイルを追加

---
+++ prepare.py
@@ -0,0 +1,35 @@
+import os
+import csv
+
+from glob import glob
+from sklearn.model_selection import train_test_split
+
+
+FLOWERS_IMAGES = './images/'
+
+FLOWER_NAME = ['Tulip', 'Snowdrop', 'LilyValley', 'Bluebell', 'Crocus', 'Iris', 'Tigerlily', 'Daffodil', 'Fritillary', 'Sunflower', 'Daisy', 'ColtsFoot', 'Dandelion', 'Cowslip', 'Buttercup', 'Windflower', 'Pansy']
+
+
+def main():
+    data = []
+
+    # get pairs with filename and signal
+    for index, image in enumerate(sorted(glob(os.path.join(FLOWERS_IMAGES, '*')))):
+        data.append((FLOWER_NAME[index // 80], image))
+
+    # split data for train or test
+    train, test = train_test_split(data, random_state=2017)
+
+    # create train dataset
+    with open('train.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(train)
+
+    # create test dataset
+    with open('test.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(test)
+
+
+if __name__ == '__main__':
+    main()
